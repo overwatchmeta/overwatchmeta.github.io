f8ec09297cc489fb13861a9ca0a896e845fe11cd
willkuhns <willkuhns2@gmail.com>
2017-08-10 14:00:28 -0700

    1.13 added

--- index/heroLoad.js
+++ index/heroLoad.js
@@ -1,3 +1,26 @@
-var genji= new Object(),
-genji.isSelfCounter = true,
-genji.counters = [winston,mei,zarya,symmetra];
+
+/*
+<div class="character-container" ontouchstart="this.classList.toggle('hover');">
+						<div class="flipper">
+					<div class="character-front" id="torbjorn">
+						<h5>Torbjorn</h5>
+						<img src="../imgs/Torbjorn.png"></img>
+					</div>
+					<div class="character-back" id="torbjorn">
+									<p> <b>PRO</b> </p>
+								<div class="Pro-Con-List">
+            			   		   1. Turrent's range<br>
+            			   		   2. High weapon damage with low falloff<br>
+            			   		   3. Armor on supports<br>
+            			   		   4. Ulting with turrent up stops most pushes
+								</div>
+								<p> <b>CON</b></p>
+								<div class="Pro-Con-List">
+									1. Easy to headshot
+									
+								</div>
+							</div>
+						</div>
+					</div>
+
+*/
